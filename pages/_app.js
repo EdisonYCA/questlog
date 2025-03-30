7044a98e20b3c279e3aabe98e4e6af46684a00ba
Edison Caceres <edisonyca127@gmail.com>
2025-03-30 07:12:39 -0400

    Add StateContext provider to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import { StateContext } from "@/context/StateContent";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
+    <StateContext>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </StateContext>
   );
 }
